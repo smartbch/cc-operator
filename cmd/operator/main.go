47783c193deb666063019a29ed5a7042b28b3c49
zxh0 <zhangxiuhong2013@gmail.com>
2022-11-21 16:05:51 +0800

    Add help option to operator cmd

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 var (
+	helpFlag        = false
 	serverName      = "sbch-operator"
 	listenAddr      = "0.0.0.0:8801"
 	bootstrapRpcURL = "http://localhost:8545"
@@ -17,12 +18,18 @@ var (
 )
 
 func main() {
+	flag.BoolVar(&helpFlag, "help", false, "show help")
 	flag.StringVar(&serverName, "serverName", "cc-operator", "server name to generate TLS certificate")
 	flag.StringVar(&listenAddr, "listenAddr", "0.0.0.0:8801", "listen addr, ip:port")
 	flag.StringVar(&bootstrapRpcURL, "bootstrapRpcURL", "http://localhost:8545", "bootstrap smartBCH RPC URL")
 	flag.StringVar(&nodesGovAddr, "nodesGovAddr", "0x0000000000000000000000000000000000001234", "address of NodesGov contract")
 	flag.StringVar(&monitorAddrList, "monitorAddrList", "", "comma separated monitor addresses")
 	flag.Parse()
+
+	if helpFlag {
+		flag.Usage()
+		return
+	}
 
 	operator.Start(serverName, listenAddr, bootstrapRpcURL, nodesGovAddr, monitorAddrList)
 }
