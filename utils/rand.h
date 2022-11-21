059ca7aedfc12b98c0dae91a26e6f546fccdef84
zxh0 <zhangxiuhong2013@gmail.com>
2022-11-21 14:50:02 +0800

    Update rand.h
    adapt ARM

--- utils/rand.h
+++ utils/rand.h
@@ -1,9 +1,42 @@
 #include <stdint.h>
 
-#define RETRY_LIMIT 10
+#define RETRY_LIMIT 100
 
 #define DRNG_SUCCESS 1
 #define DRNG_NOT_READY -1
+
+#if defined(__arm__) || defined(__aarch32__) || defined(__arm64__) || defined(__aarch64__) || defined(_M_ARM)
+
+#define __ARM_FEATURE_RNG
+#include <arm_acle.h>
+
+int rdrand_16(uint16_t* x, int retry)
+{
+	uint64_t r;
+	if (retry)
+	{
+		for (int i = 0; i < RETRY_LIMIT; i++)
+		{
+			if (__rndr(&r)==0) {
+				*x = (uint16_t)r;
+				return DRNG_SUCCESS;
+			}
+		}
+
+		return DRNG_NOT_READY;
+	}
+	else
+	{
+			if (__rndr(&r)==0) {
+				*x = (uint16_t)r;
+				return DRNG_SUCCESS;
+			} else
+				return DRNG_NOT_READY;
+	}
+}
+
+
+#else
 
 #define _rdrand_step(x) ({ unsigned char err; asm volatile("rdrand %0; setc %1":"=r"(*x), "=qm"(err)); err; })
 
@@ -30,3 +63,5 @@ int rdrand_16(uint16_t* x, int retry)
 					return DRNG_NOT_READY;
 		}
 }
+
+#endif
